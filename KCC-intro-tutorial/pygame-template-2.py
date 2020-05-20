377c6c4514aa20d17b31d3306f64346459c341ed
bheki-maenetja <bhekimaenetja@gmail.com>
2020-05-20 11:16:53 +0200

    created new pygame template

---
+++ KCC-intro-tutorial/pygame-template-2.py
@@ -0,0 +1,71 @@
+# pylint: disable=no-member
+# pylint: disable=too-many-function-args
+# Skeleton for a new project
+import pygame
+import random
+from os import path
+
+# MAIN FUNCTION
+def main(): 
+  # INITIALISE PYGAME AND CREATE WINDOW
+  WIDTH = 360
+  HEIGHT = 480
+  FPS = 30
+
+  pygame.init()
+  pygame.mixer.init()
+  screen = pygame.display.set_mode((WIDTH, HEIGHT))
+  pygame.display.set_caption("My Game")
+  clock = pygame.time.Clock()
+
+  # GAME LOOP
+  running = True
+
+  while running:
+    # Process Input (events)
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT: # check for closing the window
+        running = False
+
+    # Update
+    all_sprites.update()
+    
+    # Draw / Render
+    screen.fill(BLUE)
+    all_sprites.draw(screen)
+
+    # AFTER Drawing Everything, Flip the Display
+    pygame.display.flip()
+
+    # Keep Game Loop Running at Given FPS
+    clock.tick(FPS)
+
+# USEFUL COLOURS
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+GREEN = (0, 255, 0)
+ORANGE = (255, 165, 0)
+MAGENTA = (255, 0, 255)
+OLIVE = (128, 128, 0)
+KHAKI = (240, 230, 140)
+TEAL = (0, 128, 128)
+SIENNA = (160, 83, 45)
+TAN = (210, 180, 140)
+INDIGO = (75, 0, 130)
+
+# LOAD ALL SPRITE IMAGES
+img_dir = path.join(path.dirname(__file__), 'img')
+
+# SETUP SPRITES
+all_sprites = pygame.sprite.Group()
+
+# GAME VARIABLES
+
+# TEXT
+
+# UTILITY FUNCTIONS
+
+if __name__ == '__main__': main()
